5940ff4563f9c24691a39dbe18a4e77df477606c
Rowan Miller <mail@romiller.com>
2016-03-15 15:09:47 -0700

    First cut of temperature widget

---
+++ src/RocketProfiler.UI/ViewModels/TemperatureSensorWidgetViewModel.cs
@@ -0,0 +1,39 @@
+﻿using RocketProfiler.Controller;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RocketProfiler.UI.ViewModels
+{
+    public class TemperatureSensorWidgetViewModel : INotifyPropertyChanged
+    {
+        private readonly Sensor _sensor;
+
+        public TemperatureSensorWidgetViewModel(Sensor sensor)
+        {
+            _sensor = sensor;
+
+            _sensor.LastRead.PropertyChanged += (_, __) =>
+            {
+                OnPropertyChanged(nameof(CelsiusText));
+                OnPropertyChanged(nameof(FarenheightText));
+                OnPropertyChanged(nameof(ThermomoterValue));
+            };
+        }
+
+        public string CelsiusText => (int)_sensor.LastRead.Value.Value + "°C";
+
+        public string FarenheightText => (int)(_sensor.LastRead.Value.Value * 9.0 / 5.0 + 32) + "°F";
+
+        public int ThermomoterValue => (int)(_sensor.LastRead.Value.Value * 100 / _sensor.MaxValue);
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ src/RocketProfiler.UI/Views/TemperatureSensorWidget.xaml.cs
@@ -0,0 +1,31 @@
+﻿using RocketProfiler.Controller;
+using RocketProfiler.UI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RocketProfiler.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for TemperatureSensorWidget.xaml
+    /// </summary>
+    public partial class TemperatureSensorWidget : UserControl
+    {
+        public TemperatureSensorWidget(TemperatureSensorWidgetViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+    }
+}
