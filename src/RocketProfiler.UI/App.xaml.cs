9ff4cdc65f01f3fd313c80a0679a88a635023dbe
Arthur Vickers <ajcvickers@hotmail.com>
2016-03-15 10:22:54 -0700

    R# stuff

--- src/RocketProfiler.UI/App.xaml.cs
+++ src/RocketProfiler.UI/App.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System.Windows;
+﻿// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Windows;
 
 namespace RocketProfiler.UI
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    ///     Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
--- src/RocketProfiler.UI/Views/MainWindow.xaml.cs
+++ src/RocketProfiler.UI/Views/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System.Windows;
+﻿// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Windows;
 
 namespace RocketProfiler.UI.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
